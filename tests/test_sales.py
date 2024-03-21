81167680530e47cdb4c1c56afebe0c9a50ba6fe1
Matheus Tenório <matheusct16@gmail.com>
2024-03-21 14:11:17 -0300

    0.3.0

--- tests/test_sales.py
+++ tests/test_sales.py
@@ -2,14 +2,17 @@ import unittest
 from unittest.mock import patch
 from hotmart_python import Hotmart
 
+client_id = 'b32450c1-1352-246a-b6d3-d49d6db815ea'
+client_secret = '90bcc221-cebd-5a5b-00e2-72cab47d9282'
+basic = ('Basic YjIzNTQxYzAtMyEzNS20MjVhLWI1ZDItZDM4ZDVkYjcwNGVhOjA5Y2JiMTEzLWRiZWMtNGI0YS05OWUxLT\
+I3Y2FiNDdkOTI4Mg==')
+
 
 class TestHotmart(unittest.TestCase):
     def setUp(self):
-        self.hotmart = Hotmart(client_id='b32450c1-1352-246a-b6d3-d49d6db815ea',
-                               client_secret='90bcc221-cebd-5a5b-00e2-72cab47d9282',
-                               basic='Basic '
-                                     'YjIzNTQxYzAtMyEzNS20MjVhLWI1ZDItZDM4ZDVkYjcwNGVhOjA5Y2JiMTEzLWRiZWMtNGI0YS05OWUx'
-                                     'LTI3Y2FiNDdkOTI4Mg==')
+        self.hotmart = Hotmart(client_id=client_id,
+                               client_secret=client_secret,
+                               basic=basic)
 
     @patch.object(Hotmart, '_build_payload')
     @patch.object(Hotmart, '_pagination')
@@ -17,11 +20,12 @@ class TestHotmart(unittest.TestCase):
         mock_build_payload.return_value = {"transaction_status": "approved"}
         mock_pagination.return_value = [{"id": 1}, {"id": 2}]
         result = self.hotmart.get_sales_history(paginate=True, transaction_status="approved")
+
         self.assertEqual(result, [{"id": 1}, {"id": 2}])
 
     @patch.object(Hotmart, '_build_payload')
     @patch.object(Hotmart, '_pagination')
-    def test_get_sales_history_without_pagination(self, mock_pagination, mock_build_payload):
+    def test_get_sales_history_no_pagination(self, mock_pagination, mock_build_payload):
         mock_build_payload.return_value = {"filter1": "value1"}
         mock_pagination.return_value = [{"id": 1}, {"id": 2}]
         result = self.hotmart.get_sales_history(paginate=False, filter1="value1")
@@ -33,100 +37,188 @@ class TestHotmart(unittest.TestCase):
         mock_build_payload.return_value = {"filter1": "value1"}
         mock_pagination.return_value = None
         result = self.hotmart.get_sales_history(paginate=True, filter1="value1")
+
         self.assertIsNone(result)
 
     @patch.object(Hotmart, '_build_payload')
     @patch.object(Hotmart, '_pagination')
     def test_get_sales_summary_with_pagination(self, mock_pagination, mock_build_payload):
-        mock_build_payload.return_value = {"filter1": "value1"}
-        mock_pagination.return_value = [{"id": 1}, {"id": 2}]
+        mock_build_payload.return_value = {
+            "filter1": "value1"
+        }
+        mock_pagination.return_value = [
+            {"id": 1},
+            {"id": 2}
+        ]
         result = self.hotmart.get_sales_summary(paginate=True, filter1="value1")
-        self.assertEqual(result, [{"id": 1}, {"id": 2}])
+
+        self.assertEqual(result, [
+            {"id": 1},
+            {"id": 2}
+        ])
 
     @patch.object(Hotmart, '_build_payload')
     @patch.object(Hotmart, '_pagination')
-    def test_get_sales_summary_without_pagination(self, mock_pagination, mock_build_payload):
-        mock_build_payload.return_value = {"filter1": "value1"}
-        mock_pagination.return_value = [{"id": 1}, {"id": 2}]
+    def test_get_sales_summary_no_pagination(self, mock_pagination, mock_build_payload):
+        mock_build_payload.return_value = {
+            "filter1": "value1"
+        }
+        mock_pagination.return_value = [
+            {"id": 1},
+            {"id": 2}
+        ]
         result = self.hotmart.get_sales_summary(paginate=False, filter1="value1")
-        self.assertEqual(result, [{"id": 1}, {"id": 2}])
+
+        self.assertEqual(result, [
+            {"id": 1},
+            {"id": 2}
+        ])
 
     @patch.object(Hotmart, '_build_payload')
     @patch.object(Hotmart, '_pagination')
     def test_get_sales_summary_with_failed_pagination(self, mock_pagination, mock_build_payload):
-        mock_build_payload.return_value = {"filter1": "value1"}
+        mock_build_payload.return_value = {
+            "filter1": "value1"
+        }
         mock_pagination.return_value = None
         result = self.hotmart.get_sales_summary(paginate=True, filter1="value1")
+
         self.assertIsNone(result)
 
     @patch.object(Hotmart, '_build_payload')
     @patch.object(Hotmart, '_pagination')
-    def test_sales_participants_retrieval_with_pagination(self, mock_pagination, mock_build_payload):
-        mock_build_payload.return_value = {"filter1": "value1"}
-        mock_pagination.return_value = [{"id": 1}, {"id": 2}]
+    def test_sales_participants_retrieval_with_pagination(self, mock_pagination,
+                                                          mock_build_payload):
+        mock_build_payload.return_value = {
+            "filter1": "value1"
+        }
+        mock_pagination.return_value = [
+            {"id": 1},
+            {"id": 2}
+        ]
         result = self.hotmart.get_sales_participants(paginate=True, filter1="value1")
-        self.assertEqual(result, [{"id": 1}, {"id": 2}])
+
+        self.assertEqual(result, [
+            {"id": 1},
+            {"id": 2}
+        ])
 
     @patch.object(Hotmart, '_build_payload')
     @patch.object(Hotmart, '_pagination')
-    def test_sales_participants_retrieval_without_pagination(self, mock_pagination, mock_build_payload):
-        mock_build_payload.return_value = {"filter1": "value1"}
-        mock_pagination.return_value = [{"id": 1}, {"id": 2}]
+    def test_sales_participants_retrieval_no_pagination(self, mock_pagination, mock_build_payload):
+        mock_build_payload.return_value = {
+            "filter1": "value1"
+        }
+        mock_pagination.return_value = [
+            {"id": 1},
+            {"id": 2}
+        ]
         result = self.hotmart.get_sales_participants(paginate=False, filter1="value1")
-        self.assertEqual(result, [{"id": 1}, {"id": 2}])
+
+        self.assertEqual(result, [
+            {"id": 1},
+            {"id": 2}
+        ])
 
     @patch.object(Hotmart, '_build_payload')
     @patch.object(Hotmart, '_pagination')
-    def test_sales_participants_retrieval_with_failed_pagination(self, mock_pagination, mock_build_payload):
-        mock_build_payload.return_value = {"filter1": "value1"}
+    def test_sales_participants_retrieval_failed_pagination(self, mock_pagination,
+                                                            mock_build_payload):
+        mock_build_payload.return_value = {
+            "filter1": "value1"
+        }
         mock_pagination.return_value = None
         result = self.hotmart.get_sales_participants(paginate=True, filter1="value1")
+
         self.assertIsNone(result)
 
     @patch.object(Hotmart, '_build_payload')
     @patch.object(Hotmart, '_pagination')
     def test_sales_commissions_retrieval_with_pagination(self, mock_pagination, mock_build_payload):
-        mock_build_payload.return_value = {"filter1": "value1"}
-        mock_pagination.return_value = [{"id": 1}, {"id": 2}]
+        mock_build_payload.return_value = {
+            "filter1": "value1"
+        }
+        mock_pagination.return_value = [
+            {"id": 1},
+            {"id": 2}
+        ]
         result = self.hotmart.get_sales_commissions(paginate=True, filter1="value1")
-        self.assertEqual(result, [{"id": 1}, {"id": 2}])
+
+        self.assertEqual(result, [
+            {"id": 1},
+            {"id": 2}
+        ])
 
     @patch.object(Hotmart, '_build_payload')
     @patch.object(Hotmart, '_pagination')
-    def test_sales_commissions_retrieval_without_pagination(self, mock_pagination, mock_build_payload):
-        mock_build_payload.return_value = {"filter1": "value1"}
-        mock_pagination.return_value = [{"id": 1}, {"id": 2}]
+    def test_sales_commissions_retrieval_no_pagination(self, mock_pagination, mock_build_payload):
+        mock_build_payload.return_value = {
+            "filter1": "value1"
+        }
+        mock_pagination.return_value = [
+            {"id": 1},
+            {"id": 2}
+        ]
         result = self.hotmart.get_sales_commissions(paginate=False, filter1="value1")
-        self.assertEqual(result, [{"id": 1}, {"id": 2}])
+
+        self.assertEqual(result, [
+            {"id": 1},
+            {"id": 2}
+        ])
 
     @patch.object(Hotmart, '_build_payload')
     @patch.object(Hotmart, '_pagination')
-    def test_sales_commissions_retrieval_with_failed_pagination(self, mock_pagination, mock_build_payload):
-        mock_build_payload.return_value = {"filter1": "value1"}
+    def test_sales_commissions_retrieval_failed_pagination(self, mock_pagination,
+                                                           mock_build_payload):
+        mock_build_payload.return_value = {
+            "filter1": "value1"
+        }
         mock_pagination.return_value = None
         result = self.hotmart.get_sales_commissions(paginate=True, filter1="value1")
+
         self.assertIsNone(result)
 
     @patch.object(Hotmart, '_build_payload')
     @patch.object(Hotmart, '_pagination')
-    def test_sales_price_details_retrieval_with_pagination(self, mock_pagination, mock_build_payload):
-        mock_build_payload.return_value = {"filter1": "value1"}
-        mock_pagination.return_value = [{"id": 1}, {"id": 2}]
+    def test_sales_price_details_retrieval_with_pagination(self, mock_pagination,
+                                                           mock_build_payload):
+        mock_build_payload.return_value = {
+            "filter1": "value1"
+        }
+        mock_pagination.return_value = [
+            {"id": 1},
+            {"id": 2}
+        ]
         result = self.hotmart.get_sales_price_details(paginate=True, filter1="value1")
-        self.assertEqual(result, [{"id": 1}, {"id": 2}])
+        self.assertEqual(result, [
+            {"id": 1},
+            {"id": 2}
+        ])
 
     @patch.object(Hotmart, '_build_payload')
     @patch.object(Hotmart, '_pagination')
-    def test_sales_price_details_retrieval_without_pagination(self, mock_pagination, mock_build_payload):
-        mock_build_payload.return_value = {"filter1": "value1"}
-        mock_pagination.return_value = [{"id": 1}, {"id": 2}]
+    def test_sales_price_details_retrieval_no_pagination(self, mock_pagination, mock_build_payload):
+        mock_build_payload.return_value = {
+            "filter1": "value1"
+        }
+        mock_pagination.return_value = [
+            {"id": 1},
+            {"id": 2}
+        ]
         result = self.hotmart.get_sales_price_details(paginate=False, filter1="value1")
-        self.assertEqual(result, [{"id": 1}, {"id": 2}])
+        self.assertEqual(result, [
+            {"id": 1},
+            {"id": 2}
+        ])
 
     @patch.object(Hotmart, '_build_payload')
     @patch.object(Hotmart, '_pagination')
-    def test_sales_price_details_retrieval_with_failed_pagination(self, mock_pagination, mock_build_payload):
-        mock_build_payload.return_value = {"filter1": "value1"}
+    def test_sales_price_details_retrieval_failed_pagination(self, mock_pagination,
+                                                             mock_build_payload):
+        mock_build_payload.return_value = {
+            "filter1": "value1"
+        }
         mock_pagination.return_value = None
         result = self.hotmart.get_sales_price_details(paginate=True, filter1="value1")
+
         self.assertIsNone(result)
--- tests/test_subscriptions.py
+++ tests/test_subscriptions.py
@@ -2,14 +2,17 @@ import unittest
 from unittest.mock import patch
 from hotmart_python import Hotmart, RequestException
 
+client_id = 'b32450c1-1352-246a-b6d3-d49d6db815ea'
+client_secret = '90bcc221-cebd-5a5b-00e2-72cab47d9282'
+basic = ('Basic YjIzNTQxYzAtMyEzNS20MjVhLWI1ZDItZDM4ZDVkYjcwNGVhOjA5Y2JiMTEz'
+         'LWRiZWMtNGI0YS05OWUxLTI3Y2FiNDdkOTI4Mg==')
+
 
 class TestHotmart(unittest.TestCase):
     def setUp(self):
-        self.hotmart = Hotmart(client_id='b32450c1-1352-246a-b6d3-d49d6db815ea',
-                               client_secret='90bcc221-cebd-5a5b-00e2-72cab47d9282',
-                               basic='Basic '
-                                     'YjIzNTQxYzAtMyEzNS20MjVhLWI1ZDItZDM4ZDVkYjcwNGVhOjA5Y2JiMTEzLWRiZWMtNGI0YS05OWUx'
-                                     'LTI3Y2FiNDdkOTI4Mg==')
+        self.hotmart = Hotmart(client_id=client_id,
+                               client_secret=client_secret,
+                               basic=basic)
 
     @patch.object(Hotmart, '_request_with_token')
     def should_retrieve_subscriptions_when_valid_request(self, mock_request_with_token):
@@ -30,7 +33,8 @@ class TestHotmart(unittest.TestCase):
         self.assertEqual(result, {"summary": {"total": 10}})
 
     @patch.object(Hotmart, '_request_with_token')
-    def should_raise_exception_when_retrieving_subscription_summary_fails(self, mock_request_with_token):
+    def should_raise_exception_when_retrieving_subscription_summary_fails(self,
+                                                                          mock_request_with_token):
         mock_request_with_token.side_effect = RequestException("Error", "url")
         with self.assertRaises(RequestException):
             self.hotmart.get_subscriptions_summary(paginate=False)
@@ -42,7 +46,9 @@ class TestHotmart(unittest.TestCase):
         self.assertEqual(result, {"purchases": [{"id": 1}, {"id": 2}]})
 
     @patch.object(Hotmart, '_request_with_token')
-    def should_raise_exception_when_retrieving_subscription_purchases_fails(self, mock_request_with_token):
+    def should_raise_exception_when_retrieving_subscription_purchases_fails(self,
+                                                                            mock_request_with_token
+                                                                            ):
         mock_request_with_token.side_effect = RequestException("Error", "url")
         with self.assertRaises(RequestException):
             self.hotmart.get_subscription_purchases(subscriber_code="123", paginate=False)
@@ -62,11 +68,13 @@ class TestHotmart(unittest.TestCase):
     @patch.object(Hotmart, '_request_with_token')
     def should_reactivate_and_charge_subscription_when_valid_request(self, mock_request_with_token):
         mock_request_with_token.return_value = {"status": "reactivated"}
-        result = self.hotmart.reactivate_and_charge_subscription(subscriber_code=["123"], charge=True)
+        result = self.hotmart.reactivate_and_charge_subscription(subscriber_code=["123"],
+                                                                 charge=True)
         self.assertEqual(result, {"status": "reactivated"})
 
     @patch.object(Hotmart, '_request_with_token')
-    def should_raise_exception_when_reactivating_and_charging_subscription_fails(self, mock_request_with_token):
+    def raise_exception_when_reactivating_and_charging_subscription_fails(self,
+                                                                          mock_request_with_token):
         mock_request_with_token.side_effect = RequestException("Error", "url")
         with self.assertRaises(RequestException):
             self.hotmart.reactivate_and_charge_subscription(subscriber_code=["123"], charge=True)
