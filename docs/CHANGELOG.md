8fc04fa07b331066e2a3fd67bc86d525b6cb6e4e
Matheus Tenório <matheusct16@gmail.com>
2024-03-20 22:55:04 -0300

    New subscriptions endpoint + Tests + changes in requests functions

---
+++ docs/CHANGELOG.md
@@ -0,0 +1,11 @@
+0.1.20 / 2224-03-20
+==================
+
+  * Added subscriptions endpoint
+  * Added tests for subscriptions
+  * Changed _get_with_token and _post_with_token methods to use the new _request_with_token for more flexibility
+
+0.1.19 / 2024
+==================
+
+  * Test releases
--- hotmart_python/hotmart.py
+++ hotmart_python/hotmart.py
@@ -56,7 +56,7 @@ class Hotmart:
         self.basic = basic
         self.sandbox = sandbox
         self.base_url = SANDBOX_BASE_URL if sandbox else PRODUCTION_BASE_URL
-        self.base_url = f'{self.base_url}v{api_version}/'
+        self.base_url = f'{self.base_url}v{api_version}'
 
         # Token caching and some logic to do better logging.
         self.token_cache = None
@@ -87,7 +87,8 @@ class Hotmart:
         return self.logger.warning(f"Instance in {'Sandbox' if self.sandbox else 'Production'} mode")
 
     def _make_request(self, method: Any, url: str, headers: Optional[Dict[str, str]] = None,
-                      params: Optional[Dict[str, Any]] = None, log_level: int = None) -> Optional[Dict[str, Any]]:
+                      params: Optional[Dict[str, Any]] = None, body: Optional[Dict[str, str]] = None,
+                      log_level: int = None) -> Optional[Dict[str, Any]]:
         """
         Makes a request to the given url.
         :param method: The request method (e.g, requests.get, requests.post).
@@ -105,7 +106,8 @@ class Hotmart:
             self.logger.debug(f"Request URL: {url}")
             self.logger.debug(f"Request headers: {headers}")
             self.logger.debug(f"Request params: {params}")
-            response = method(url, headers=headers, params=params)
+            self.logger.debug(f"Request body: {body}")
+            response = method(url, headers=headers, params=params, data=body)
             self.logger.debug(f"Response content: {response.text}")
             response.raise_for_status()
             return response.json()
@@ -168,21 +170,36 @@ class Hotmart:
             self.token_found_in_cache = False
         return token
 
-    def _get_with_token(self, url: str, params: Optional[Dict[str, str]] = None) -> Optional[Dict[str, Any]]:
+    def _request_with_token(self, method: str, url: str, body: Optional[Dict[str, Any]] = None, params: Optional[Dict[str, Any]] = None) -> Optional[Dict[str, Any]]:
         """
-        Makes authenticated a GET request to the specified URL with the given params.
+        Makes an authenticated request (GET, POST, PATCH, etc.) to the specified URL with the given body or params.
+        :param method: The HTTP method (e.g., 'GET', 'POST', 'PATCH').
         :param url: the URL to make the request to.
+        :param body: Optional request body.
         :param params: Optional request parameters.
         :return: The JSON Response if successful, otherwise None.
         """
         token = self._get_token()
         if token is None:
             return None
-        headers = {'Authorization': f'Bearer {token}'}
-        return self._make_request(requests.get, url, headers=headers, params=params)
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': f'Bearer {token}'
+        }
+    
+        method_mapping = {
+            'GET': requests.get,
+            'POST': requests.post,
+            'PATCH': requests.patch
+        }
+    
+        if method.upper() not in method_mapping:
+            self.logger.error(f"Unsupported method: {method}")
+            return None
+    
+        return self._make_request(method_mapping[method.upper()], url, headers=headers, params=params, body=body)
 
-    def _pagination(self, url: str, params: Optional[Dict[str, Any]] = None, paginate: bool = False) -> Optional[
-            List[Dict[str, Any]]]:
+    def _pagination(self, method: str, url: str, params: Optional[Dict[str, Any]] = None, paginate: bool = False) -> Optional[List[Dict[str, Any]]]:
         """
         Retrieves all pages of data for a paginated endpoint.
         :param url: The URL of the paginated endpoint.
@@ -191,11 +208,11 @@ class Hotmart:
         :return: A list containing data from all pages, or None if an error occurred.
         """
         if not paginate:
-            return self._get_with_token(url, params=params)
+            return self._request_with_token(method=method, url=url, params=params)
         all_items = []
 
         self.logger.info("Fetching first page...")
-        response = self._get_with_token(url, params=params)
+        response = self._request_with_token(method=method, url=url, params=params)
 
         if response is None:
             self.logger.error("Failed to fetch first page.")
@@ -207,7 +224,7 @@ class Hotmart:
             next_page_token = response["page_info"]["next_page_token"]
             self.logger.info(f"Fetching next page with token: {next_page_token}")
             params["page_token"] = next_page_token
-            response = self._get_with_token(url, params=params)
+            response = self._request_with_token(url, params=params)
 
             if response is None:
                 self.logger.error("Failed to fetch next page.")
@@ -227,9 +244,10 @@ class Hotmart:
         :return: Sales history data if available, otherwise None.
         """
         self._log_instance_mode()
-        method_url = f'{self.base_url}/sales/history'
+        method = "get"
+        url = f'{self.base_url}/sales/history'
         payload = self._build_payload(**kwargs)
-        return self._pagination(method_url, params=payload, paginate=paginate)
+        return self._pagination(method=method, url=url, params=payload, paginate=paginate)
 
     def get_sales_summary(self, paginate: bool = False, **kwargs: Any) -> Optional[Dict[str, Any]]:
         """
@@ -242,9 +260,10 @@ class Hotmart:
         """
 
         self._log_instance_mode()
-        method_url = f'{self.base_url}/sales/summary'
+        method = "get"
+        url = f'{self.base_url}/sales/summary'
         payload = self._build_payload(**kwargs)
-        return self._pagination(method_url, params=payload, paginate=paginate)
+        return self._pagination(method=method, url=url, params=payload, paginate=paginate)
 
     def get_sales_users(self, paginate: bool = True, **kwargs: Any) -> Optional[Dict[str, Any]]:
         """
@@ -257,9 +276,10 @@ class Hotmart:
         """
 
         self._log_instance_mode()
-        method_url = f'{self.base_url}/sales/users'
+        method = "get"
+        url = f'{self.base_url}/sales/users'
         payload = self._build_payload(**kwargs)
-        return self._pagination(method_url, params=payload, paginate=paginate)
+        return self._pagination(method=method, url=url, params=payload, paginate=paginate)
 
     def get_sales_commissions(self, paginate: bool = False, **kwargs: Any) -> Optional[Dict[str, Any]]:
         """
@@ -272,9 +292,10 @@ class Hotmart:
         """
 
         self._log_instance_mode()
-        method_url = f'{self.base_url}/sales/commissions'
+        method = "get"
+        url = f'{self.base_url}/sales/commissions'
         payload = self._build_payload(**kwargs)
-        return self._pagination(method_url, params=payload, paginate=paginate)
+        return self._pagination(method=method, url=url, params=payload, paginate=paginate)
 
     def get_sales_price_details(self, paginate: bool = False, **kwargs: Any) -> Optional[Dict[str, Any]]:
         """
@@ -287,9 +308,10 @@ class Hotmart:
         """
 
         self._log_instance_mode()
-        method_url = f'{self.base_url}/sales/price/details'
+        method = "get"
+        url = f'{self.base_url}/sales/price/details'
         payload = self._build_payload(**kwargs)
-        return self._pagination(method_url, params=payload, paginate=paginate)
+        return self._pagination(method=method, url=url, params=payload, paginate=paginate)
 
     def get_subscriptions(self, paginate: bool = False, **kwargs: Any) -> Optional[Dict[str, Any]]:
         """
@@ -302,6 +324,90 @@ class Hotmart:
         """
 
         self._log_instance_mode()
-        method_url = f'{self.base_url}/subscriptions'
+        method = "get"
+        url = f'{self.base_url}/subscriptions'
         payload = self._build_payload(**kwargs)
-        return self._pagination(method_url, params=payload, paginate=paginate)
+        return self._pagination(method=method, url=url, params=payload, paginate=paginate)
+
+    def get_subscriptions_summary(self, paginate: bool = False, **kwargs: Any) -> Optional[Dict[str, Any]]:
+        """
+        Retrieves subscription summary data based on the provided filters.
+        :param paginate: Whether to paginate the results or not (default is False).
+        :param kwargs: Filters to apply on the request. Expected kwargs can be found in the "Request parameters"
+        section of the API Docs.
+        :return: Subscription data if available, otherwise None.
+        """
+
+        self._log_instance_mode()
+        method = "get"
+        url = f'{self.base_url}/subscriptions/summary'
+        payload = self._build_payload(**kwargs)
+        return self._pagination(method=method, url=url, params=payload, paginate=paginate)
+
+    def get_subscription_purchases(self, subscriber_code, paginate: bool = False, **kwargs: Any) -> Optional[Dict[str, Any]]:
+        """
+        Retrieves subscription purchases data based on the provided filters.
+
+        :param subscriber_code: The subscriber code to filter the request.
+        :param kwargs: Filters to apply on the request. Expected kwargs can be found in the "Request parameters"
+        section of the API Docs.
+        :param paginate: Whether to paginate the results or not (default is False).
+        :return: Subscription purchases data if available, otherwise None.
+        """
+
+        self._log_instance_mode()
+        method = "get"
+        url = f'{self.base_url}/subscriptions/{subscriber_code}/purchases'
+        payload = self._build_payload(**kwargs)
+        return self._pagination(method=method, url=url, params=payload, paginate=paginate)
+
+    def cancel_subscription(self, subscriber_code: list[str], send_email: bool = True) -> Optional[Dict[str, Any]]:
+        """
+        Cancels a subscription.
+        :param subscriber_code: The subscriber code you want to cancel the subscription
+        :param send_email: Whether to email the subscriber or not (default is True).
+        :return:
+        """
+
+        self._log_instance_mode()
+        method = "post"
+        url = f'{self.base_url}/subscriptions/cancel'
+        payload = {
+            "subscriber_code": subscriber_code,
+            "send_email": send_email
+        }
+        return self._request_with_token(method=method, url=url, body=payload)
+
+    def reactivate_and_charge_subscription(self, subscriber_code: list[str], charge: bool = False) -> Optional[Dict[str, Any]]:
+        """
+        Reactivates and charges a subscription.
+        :param subscriber_code: The subscriber code you want to reactivate and charge the subscription
+        :param charge: Whether to make a new charge to the subscriber or not (default is False).
+        :return:
+        """
+
+        self._log_instance_mode()
+        method = "post"
+        url = f'{self.base_url}/subscriptions/reactivate'
+        payload = {
+            "subscriber_code": subscriber_code,
+            "charge": charge
+        }
+        return self._request_with_token(method=method, url=url, body=payload)
+
+    def change_due_day(self, subscriber_code: str, new_due_day: int) -> Optional[Dict[str, Any]]:
+        """
+        Changes the due day of a subscription.
+        :param subscriber_code: The subscriber code you want to change the due day
+        :param new_due_day: The new due day you want to set
+        :return:
+        """
+
+        self._log_instance_mode()
+        method = "patch"
+        url = f'{self.base_url}/subscriptions/change-due-day'
+        payload = {
+            "subscriber_code": subscriber_code,
+            "new_due_day": new_due_day
+        }
+        return self._request_with_token(method=method, url=url, body=payload)
